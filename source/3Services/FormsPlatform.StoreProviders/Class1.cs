2d9d606b79294945173320801761f08d19402860
adamsimsy <adamsimsy@gmail.com>
2015-07-24 21:31:24 +0100

    Added initial project structure

---
+++ source/3Services/FormsPlatform.StoreProviders/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FormsPlatform.StoreProviders
+{
+    public class Class1
+    {
+    }
+}
