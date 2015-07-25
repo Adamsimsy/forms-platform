746ed67355644679c2ccc377df3a87a092475ade
adamsimsy <adamsimsy@gmail.com>
2015-07-25 12:24:20 +0100

    Example of angluarjs call to webapi

---
+++ source/5Website/FormsPlatform.Website/wwwroot/scripts/controller.js
@@ -0,0 +1,27 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('app')
+        .controller('controller', controller);
+
+    controller.$inject = ['$http'];
+
+    function controller($http) {
+        activate();        
+
+        function activate() {
+
+            $http.get('/api/todo').
+              success(function (data, status, headers, config) {
+                  // this callback will be called asynchronously
+                  // when the response is available
+                  alert(data[0].Title);
+              }).
+              error(function (data, status, headers, config) {
+                  // called asynchronously if an error occurs
+                  // or server returns response with an error status.
+              });
+        }
+    }
+})();
